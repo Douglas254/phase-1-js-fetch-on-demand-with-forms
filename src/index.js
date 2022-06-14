7d681de8354123504d361ab271e80d3c0b4eb585
Douglas Obara <douglasobara97@gmail.com>
2022-06-14 22:44:03 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-document.addEventListener('DOMContentLoaded', init);
+    // Access Input Value Directly
+    const input = document.querySelector("input#searchByID");
+
+    console.log(input.value);
+
+    // Fetch Data Based on User Input
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        // Display Fetched Data on the Page
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerHTML = data.title;
+        summary.innerHTML = data.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
